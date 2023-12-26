e3aae65981543590dce0491f1938b13453daff3f
eda kes <119169166+edakes@users.noreply.github.com>
2023-12-26 16:16:45 +0300

    Add files via upload
    Nested if loop

---
+++ Program3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //User login test (kullanıcı girişi test)
+            int trial = 3;
+            while (true)
+            {
+                Console.WriteLine("please enter your nickname: ");
+                string nickname = Console.ReadLine();
+
+                Console.WriteLine("please enter your password: ");
+                string pasword = Console.ReadLine();
+
+                if (nickname == "butterfly" && pasword == "123")
+                {
+                    Console.WriteLine("correct...");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("nickname or password is incorrect");
+
+                    if (trial > 0)
+                    {
+                        Console.WriteLine("Log in again!");
+                        trial--;
+                       
+                    }
+                    if (trial == 0)
+                    {
+                        Console.WriteLine("Not allowed to log in");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
