5d15e39f773ab0a659360337864b1c4766ab1b16
eda kes <119169166+edakes@users.noreply.github.com>
2023-12-30 10:50:17 +0300

    Add files via upload

---
+++ Parametre2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parametre2
+{
+    internal class Parametre2
+    {    
+        //tek çift sayı bulma parametre ile yapımı
+        static void Main(string[] args)
+        {
+            Console.Write("değer giriniz: ");
+            
+
+            int girilenSayi = int.Parse(Console.ReadLine());
+            TekCift(girilenSayi);
+            Console.ReadLine();
+        }
+
+       
+        static int TekCift(int a)
+        {
+            if(a%2==0)
+            {
+                Console.WriteLine("sayı çifttir.");
+            }
+            else { Console.WriteLine("sayı tektir."); }
+            return a;
+        }
+    }
+}
